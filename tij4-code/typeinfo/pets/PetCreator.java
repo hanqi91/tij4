60ed493559b5a677e40268081f38e157253f893f
hanqi29 <hanqi@jd.com>
2015-11-09 18:15:06 +0800

    add 14.3

---
+++ tij4-code/typeinfo/pets/PetCreator.java
@@ -0,0 +1,31 @@
+//: typeinfo/pets/PetCreator.java
+// Creates random sequence of Pets.
+package typeinfo.pets;
+import java.utl.*;
+
+public abstract class PetCreator {
+    private Random rand = new Random(47);
+    // The List of the differenet types of Pet to create:
+    public abstract List<Class<? extends Pet>> types;
+    public Pet randomPet() {  // Create one random Pet
+        int n = rand.nextInt(types().size());
+        try {
+            return types.get(n).newInstance();
+        } catch(InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch(IllegalAccessExcepiton e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Pet[] createArray(int size) {
+        Pet[] result = new Pet[size];
+        for (int i = 0; i < size; i++)
+            result[i] = randomPet();
+        return result;
+    }
+    public ArrayList<Pet> arrayList(int size) {
+        ArrayList<Pet> result = new ArrayList<Pet>();
+        Collection.addAll(result, createArray(size));
+        return result;
+    }
+}  ///:~
