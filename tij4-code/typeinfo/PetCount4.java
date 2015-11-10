9eb7e4614004b8c12be791354d51cb038b2aeca4
hanqi29 <hanqi@jd.com>
2015-11-10 18:51:40 +0800

    Add 14.3

---
+++ tij4-code/typeinfo/PetCount4.java
@@ -0,0 +1,18 @@
+//: typeinfo/PetCount4.java
+package typeinfo;
+
+import typeinfo.pets.*;
+import net.mindview.util.*;
+import static net.mindview.util.Print.*;
+
+public class PetCount4 {
+    public static void main(String[] args) {
+        TypeCounter counter = new TypeCounter(Pet.class);
+        for (Pet pet : Pets.createArray(20)) {
+            printnb(pet.getClass().getSimpleName() + " ");
+            counter.count(pet);
+        }
+        print();
+        print(counter);
+    }
+}
