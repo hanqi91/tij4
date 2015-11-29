f19a88f65a77cb2b2252d862b8f61cb904043780
hanqi29 <hanqi@jd.com>
2015-11-29 08:58:38 +0800

    Add 15.6 15.7 15.8

---
+++ tij4-code/generics/ErasedTypeEquivalence.java
@@ -0,0 +1,10 @@
+//: generics/ErasedTypeEquivalence.java
+packge generics;
+
+import java.util.*;
+
+public class ErasedTypeEquivalence {
+    public static void main(String[] args) {
+        Class c1 = new ArrayList<String>
+    }
+}
