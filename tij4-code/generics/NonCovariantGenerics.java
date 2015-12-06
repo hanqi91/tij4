c32e7a9531f837628a20656118985f47850e7039
hanqi <hanqi91@qq.com>
2015-12-06 15:09:42 +0800

    Add 15.10

---
+++ tij4-code/generics/NonCovariantGenerics.java
@@ -0,0 +1,10 @@
+//: generics/NonCovariantGenerics.java
+// {CompileTimeError} (Won't compile)
+package generics;
+
+import java.util.*;
+
+public class NonCovariantGenerics {
+    // Compile Error: incompatible types:
+    List<Fruit> flist = new ArrayList<Apple>();
+}   ///:~
