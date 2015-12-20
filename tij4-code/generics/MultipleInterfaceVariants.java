8652dbf3a36b73a10c25266d5f9bda5c992a78f3
hanqi <hanqi91@qq.com>
2015-12-20 14:55:25 +0800

    Add 15.11.2

---
+++ tij4-code/generics/MultipleInterfaceVariants.java
@@ -0,0 +1,9 @@
+//: generics/MultipleInterfaceVariants.java
+// {CompileTimeError} (Won't compile)
+package generics;
+
+interface Payable<T> {}
+
+class Employee implements Payable<Employee> {}
+class Hourly extends Employee
+    implements Payable<Hourly> {}   ///:~
