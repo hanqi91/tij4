7b857572f891e78e59c94522383d49796398045f
hanqi29 <hanqi@jd.com>
2016-01-02 13:32:15 +0800

    Add 15.12.3

---
+++ tij4-code/generics/OrdinaryArguments.java
@@ -0,0 +1,24 @@
+//: generics/OrdinaryArguments.java
+package generics;
+
+class OrdinarySetter {
+    void set(Base base) {
+        System.out.println("OrdinarySetter.set(Base)");
+    }
+}
+
+class DerivedSetter extends OrdinarySetter {
+    void set(Derived derived) {
+        System.out.println("DerivedSetter.set(Derived)");
+    }
+}
+
+public class OrdinaryArguments {
+    public static void main(String[] arsg) {
+        Base base = new Base();
+        Derived derived = new Derived();
+        DerivedSetter ds = new DerivedSetter();
+        ds.set(derived);
+        ds.set(base);  // Compiles: overloaded, not overridden!
+    }
+}
